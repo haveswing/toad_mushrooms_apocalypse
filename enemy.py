ace6e5afc4e80e308615256bdeea615bb4972c01
haveswing <mr.osbourne@gmail.com>
2017-05-19 11:46:36 +0200

    dev.update

---
+++ enemy.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame.locals import *
+import random
+import time
+import os
+import sys
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        # Set the size, look, initial position, etc. of an enemy here...
+        pass
+
+    def update(self):
+        # Define how the enemy moves on each frame here...
+        pass
+
+
+# using example:
+
+# class Level_01(Level):
+    # def __init__(self, player):
+        # platform code already in example goes here...
+
+       # Add two enemies to the level
+       # self.enemy_list.add(Enemy())
+       # self.enemy_list.add(Enemy())
